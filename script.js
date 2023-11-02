872056ff030ee9a9461f2e660e39acfb2e001f31
Sambit Mohanty <sambit@localhost.localdomain>
2023-11-02 05:53:40 +0530

    storing cards

---
+++ script.js
@@ -0,0 +1,6 @@
+const cards = [
+    { id:1, imgSrc: 'media/playing-images/dragon.png', isFlipped: false },
+    { id:2, imgSrc: 'media/playing-images/lion.png', isFlipped: false },
+    { id:3, imgSrc: 'media/playing-images/tiger.png', isFlipped: false }
+];
+
