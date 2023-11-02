74f57aca42d5808b40edd2bc9dec1dd7eedd70fa
Sambit Mohanty <sambit@localhost.localdomain>
2023-11-02 06:28:40 +0530

    creating element and class

--- script.js
+++ script.js
@@ -14,7 +14,25 @@ var shuffleCards = () => {
 
 //rendering gameboard
 var renderBoard = () => {
-    
+    const cardContainer = document.querySelector(".card-container");
+    cardContainer.innerHTML = '';
+
+    cards.forEach ((card) => {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add(card1);
+        cardElement.dataset.id = card.id
+
+        const backImg = document.createElement("img");
+        backImg.src = card.imgSrc;
+        backImg.classList.add("back");
+
+        const frontImg = document.createElement("img");
+        frontImg.src = "media/playing-images/middle-finger.png";
+        frontImg.classList.add("front");
+
+        cardElement.appendChild(backImg);
+        cardElement.appendChild(frontImg);
+    })
 }
 
 //function to flip cards
