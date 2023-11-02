f21a8cf36f2a17e493a8843abfb5ea468c30215e
Sambit Mohanty <sambit@localhost.localdomain>
2023-11-02 06:01:12 +0530

    adding functions

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const cards = [
     { id:3, imgSrc: 'media/playing-images/tiger.png', isFlipped: false }
 ];
 
+//array to keep track of flipped cards
+let flippedCards = [];
+
+//function to shuffle the cards array
+var shuffleCards = () > {
+
+};
+
+//rendering gameboard
+var renderBoard = () => {
+    
+}
+
+//function to flip cards
+var flipCard = () => {
+
+};
+
+//function to check if two cards match
+var checkmatch = () => {
+
+};
+
+//shuffle the cards and render the initial gameboard
+shuffleCards(cards);
+renderBoard();
